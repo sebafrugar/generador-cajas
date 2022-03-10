e84a6bb72979ed41e09a9ec2247dbce6a8e4b503
sebafrugar <sebafrugar@gmail.com>
2022-03-10 19:53:31 -0400

    agregando cambio de modificacion de tamaños de cuadros

--- src/Components/Cajas.jsx
+++ src/Components/Cajas.jsx
@@ -3,28 +3,35 @@ import React, { useState, useEffect} from 'react';
 
 
 const Cajas = (props) => {
-
-    const {cajas , setCajas} = props;
-    const [color , setColor] = useState([])
-    const [height , setHeight] = useState('')
-    const [width , setWidth] = useState('')
+    const {cajas, setCajas} = props;
+    const [caja , setCaja] = useState({
+        color: '',
+        height: 0,
+        width: 0,
+    })
 
     const onSubmit = (e) =>{
         e.preventDefault();
-        setCajas([...cajas,color,height,width]);
+        setCajas([...cajas,caja])
+        console.log("funciona el boton")
     }
     useEffect(()=>{
-        console.log(cajas)
-    },[cajas]);
+        console.log(caja)
+    },[caja]);
+    
+    const handlerForm = (target) =>{
+        setCaja({...caja,[target.name]: target.value});
+        
+    }
 
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <div className='forms'>
                     <label htmlFor="box">Color: </label>
-                    <input type="color" name='box' onChange={(e) => setColor(e.target.value)} /> <br />
-                    <input type="number" name='box' onChange={(e) => setHeight(e.target.value)} /><br />
-                    <input type="number" name='box' onChange={(e) => setWidth(e.target.value)} /><br />
+                    <input type="color" name='color' onChange={(e) => handlerForm(e.target)} /> <br />
+                    <input type="number" name='height' onChange={(e) => handlerForm(e.target)} /><br />
+                    <input type="number" name='width' onChange={(e) => handlerForm(e.target)} /><br />
                 </div>    
                 <div className='forms'>
                     <input className='btn' type="submit" value='Agregar color' />
--- src/Views/Vistacajas.jsx
+++ src/Views/Vistacajas.jsx
@@ -7,9 +7,10 @@ const Vistacajas = (props) => {
 
     const {data} = props;
 
+
     return (
         <div>
-            {data.map ((caja)=><div style={{backgroundColor: caja}} className='box'></div>) }
+            {data.map ((caja,i)=><div key={i} style={{backgroundColor: caja.color, width: `${caja.width}px`, height: `${caja.height}px` }} className='box'></div>) }
         </div>
     );
 }
